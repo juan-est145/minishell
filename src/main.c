5accea0d92ca5e1fe0052d1897d175dc89146696
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-19 16:04:48 +0200

    Now it detects double and single quotes correctly

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: juan-est145 <juan-est145@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:34:35 by juan-est145       #+#    #+#             */
-/*   Updated: 2024/04/19 14:06:14 by juan-est145      ###   ########.fr       */
+/*   Updated: 2024/04/19 16:02:53 by juan-est145      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,31 @@ int	main(void)
 {
 	unsigned int i = 0;
 	unsigned int start_index = 0;
-	char *string = "echoHo'la\"";
-	const size_t	length = ft_strlen(string);
-	/*char *string = "echo";*/
+	char *string = "echo  \"Ho     ' l'a\"";
+	size_t	length;
 	t_token_list	*token_list = NULL;
+	char delimiter = '\0';
 	
 	token_list = malloc(sizeof(token_list));
 	if (*string == '\0')
 		return (free(token_list), 1);
+		
+	while (ft_isspace(*string) == true && *string != '\0')
+		string++;
+	length = ft_strlen(string);
 	while (i <= length && string[start_index] != '\0')
 	{
-		while (ft_isspace(*string) == true && *string != '\0')
-			string++;
-		if (ft_isspace(string[i]) == true || string[i] == '\0')
+		if ((ft_isspace(string[i]) == true && delimiter == '\0')|| string[i] == '\0')
 		{
-			token_list->token = ft_substr(string, start_index, i);
+			token_list->token = ft_substr(string, start_index, i - start_index);
 			start_index = i + 1;
 		}
 		else if (string[i] == '\"' || string[i] == '\'')
 		{
-			char delimiter = string[i];
-			i++;
-			while (string[i] != delimiter && i <= length)
-			{
-				i++;
-			}
-			token_list->token = ft_substr(string, start_index, i);
-			start_index = i + 1;
+			if (delimiter == '\0')
+				delimiter = string[i];
+			else if (delimiter == string[i])
+				delimiter = '\0';
 		}
 		i++;
 	}
