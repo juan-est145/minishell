ad005907fc43d5c60287547fcd2c20608ab4b61e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-22 19:54:37 +0200

    Prepared basic variables for creating abstract syntax tree

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: juan-est145 <juan-est145@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 19:15:52 by juan-est145       #+#    #+#             */
-/*   Updated: 2024/04/22 19:43:58 by juan-est145      ###   ########.fr       */
+/*   Updated: 2024/04/22 19:52:44 by juan-est145      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 t_AST	*create_ast(t_token_list **head)
 {
-	t_AST	*head;
+	t_AST			*ast_head;
+	t_token_list	**head_copy;
+}
+
+static t_AST	*precedence_climbing(int precedence, t_token_list **copy)
+{
+	t_AST				*left;
+	t_AST				*rigt;
+	int					updated_prec;
+	t_parse_identifier	current_parse;
 }
