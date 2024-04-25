9dd027b0819fe68285b2655a78d128dcf9386453
Miguel Angel <80107173+MiguelAngel456@users.noreply.github.com>
2024-04-25 15:46:58 +0200

    norminette

---
+++ src/builtings/archive_utils1.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   archive_utils1.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfuente- <mfuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 13:59:06 by mfuente-          #+#    #+#             */
+/*   Updated: 2024/04/25 14:00:26 by mfuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include "../../libft/libft.h"
+
+void	aux(char **matr, char const *s, char c)
+{
+	size_t	cont_aux;
+	size_t	cont_s;
+	size_t	cont;
+
+	cont_s = 0;
+	cont = 0;
+	while (s[cont_s])
+	{
+		cont_aux = 0;
+		while (s[cont_aux + cont_s] != 0 && s[cont_aux + cont_s] != c)
+			cont_aux++;
+		if (cont_aux > 0)
+		{
+			matr[cont] = malloc((cont_aux + 1) * sizeof(char));
+			if (!matr[cont])
+				return (liberar_memoria(matr));
+			rellenar(matr[cont], (s + cont_s), c);
+			cont++;
+			cont_s = cont_s + cont_aux;
+		}
+		else
+			cont_s++;
+	}
+	matr[cont] = NULL;
+}
+
+int	ntimes(char const *s, char c)
+{
+	int	i;
+	int	cont;
+
+	i = 0;
+	cont = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c && i != 0 && s[i - 1] != c)
+		{
+			cont++;
+		}
+		i++;
+	}
+	if (s[i - 1] != c)
+		cont++;
+	return (cont);
+}
+
+int	ft_lst_contain_change(t_lst_env **lst, char *search)
+{
+	t_lst_env	*temp;
+	int			len;
+	char		**new;
+
+	new = ft_split(search, '=');
+	len = ft_strlen(new[0]);
+	temp = *lst;
+	while (temp->next != NULL)
+	{
+		if (ft_strncmp(temp->next->text, new[0], len) == 0)
+		{
+			temp->next->text = search;
+			free_matrix(new);
+			return (0);
+		}
+		temp = temp->next;
+	}
+	free_matrix(new);
+	return (1);
+}
+
+int	ft_lst_contain(t_lst_env **lst, char *search)
+{
+	t_lst_env	*temp;
+	int			len;
+	char		**new;
+
+	new = ft_split(search, '=');
+	len = ft_strlen(new[0]);
+	temp = *lst;
+	while (temp->next != NULL)
+	{
+		if (ft_strncmp(temp->next->text, new[0], len) == 0)
+		{
+			return (0);
+		}
+		temp = temp->next;
+	}
+	return (1);
+}
+
+t_lst_env	*ft_lstnew_ms(char *content)
+{
+	t_lst_env	*nodo;
+
+	nodo = malloc(sizeof(t_lst_env));
+	if (!nodo)
+		return (0);
+	nodo->text = content;
+	nodo->next = 0;
+	return (nodo);
+}
---
+++ src/builtings/builtings1.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtings1.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfuente- <mfuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 14:01:57 by mfuente-          #+#    #+#             */
+/*   Updated: 2024/04/25 15:36:49 by mfuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include "../../libft/libft.h"
+
+// IMITA EL COMANDO PWD
+int	ft_getpwd_normi(t_lst_env *temp, int len, char **split)
+{
+	while (temp->next != NULL)
+	{
+		if (ft_strncmp(temp->next->text, "PWD", 3) == 0)
+		{
+			split = ft_split(temp->next->text, '=');
+			len = ft_strlen(split[1]);
+		}
+		temp = temp->next;
+	}
+	return (len);
+}
+
+void	ft_getpwd(char *text, t_lst_env *lst_env)
+{
+	char		*pwd;
+	char		**split;
+	t_lst_env	*temp;
+	int			len;
+
+	temp = lst_env;
+	len = ft_getpwd_normi(temp, len, split);
+	free_matrix(split);
+	split = ft_split(text, ' ');
+	pwd = malloc(sizeof(char) * len);
+	if (split[1] != NULL)
+	{
+		printf("too many arguments\n");
+		return ;
+	}
+	if (getcwd(pwd, sizeof(char) * len + 1) == NULL)
+		printf("Error al obtener el pwd\n");
+	printf("%s\n", pwd);
+	free(pwd);
+	free_matrix(split);
+}
+
+bool	ft_echo_normi(char **cmd, int i, int j, bool open)
+{
+	char	chr;
+
+	if ((cmd[i][j] == '\"' || cmd[i][j] == '\'') && open == false)
+	{
+		open = true;
+		chr = cmd[i][j];
+	}
+	else
+	{
+		if (cmd[i][j] == chr && open == true)
+		{
+			open = false;
+			chr = '\0';
+		}
+		else
+			printf("%c", cmd[i][j]);
+	}
+	return (open);
+}
+
+// IMITA LA FUNCION ECHO
+void	ft_echo(char **cmd)
+{
+	int		i;
+	int		j;
+	char	chr;
+	bool	open;
+
+	i = 1;
+	open = false;
+	if (ft_strncmp(cmd[1], "-n\0", 3) == 0)
+		i = 2;
+	while (cmd[i] != 0)
+	{
+		j = 0;
+		while (cmd[i][j] != '\0')
+		{
+			open = ft_echo_normi(cmd, i, j, open);
+			j++;
+		}
+		i++;
+		if (cmd[i] != 0)
+			printf(" ");
+	}
+	if (ft_strncmp(cmd[1], "-n\0", 3) != 0)
+		printf("\n");
+	free_matrix(cmd);
+}
+
+// IMITA EL COMANDO CD
+void	ft_cd(char *text, t_lst_env *lst_env)
+{
+	char	**split;
+	char	pwd[1024];
+	char	*aux;
+
+	split = ft_split(text, ' ');
+	if (split[2] != NULL)
+	{
+		printf("too many arguments\n ");
+	}
+	else
+	{
+		getcwd(pwd, sizeof(pwd));
+		aux = ft_fusion_string("export OLDPWD=", pwd);
+		ft_export(aux, lst_env);
+		chdir(split[1]);
+		free(aux);
+		getcwd(pwd, sizeof(pwd));
+		aux = ft_fusion_string("export PWD=", pwd);
+		ft_export(aux, lst_env);
+		free(aux);
+	}
+	free_matrix(split);
+}
