cf27b7cc320c26b1f35b00406024eac41b50608b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-27 20:26:09 +0200

    Deleted superflous commented function

--- src/builtings/archive_utils2.c
+++ src/builtings/archive_utils2.c
@@ -6,7 +6,7 @@
 /*   By: juan-est145 <juan-est145@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:00:52 by mfuente-          #+#    #+#             */
-/*   Updated: 2024/04/27 20:08:16 by juan-est145      ###   ########.fr       */
+/*   Updated: 2024/04/27 20:23:09 by juan-est145      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,6 @@ void	ft_lstadd_back_ms(t_lst_env **lst, t_lst_env *new)
 		temp->next = new;
 	}
 }
-
-/*int	get_pwd_cd_length(t_lst_env *lst_env, char *string_to_search)
-{
-	t_lst_env	*temp;
-	int			len;
-
-	len = 0;
-	temp = lst_env;
-	len = ft_getpwd_normi(temp, len, string_to_search);
-	return (len);
-}*/
 
 void	handle_cd_env(t_lst_env *lst_env, char *f(char *s1, char *s2),
 		char *export_text)
