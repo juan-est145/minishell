4a0654cc3a103ff773f07412955a4ac05868ceb6
juestrel <128648875+juan-est145@users.noreply.github.com>
2024-06-03 20:12:12 +0200

    Single pipe now does not segfaults, but it keeps waiting for input

--- src/process_ast/process_pipes_utils.c
+++ src/process_ast/process_pipes_utils.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:35:14 by juestrel          #+#    #+#             */
-/*   Updated: 2024/06/03 19:17:58 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/06/03 20:02:00 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,36 @@ void	open_pipes(t_pipex *str_pipe, t_ast *node)
 		|| node->right->simple_or_pipe == MIDDLE_PIPE)
 	{
 		pipe(str_pipe->fd_arrays[array_index]);
-        str_pipe->fd_array_num++;
+		str_pipe->fd_array_num++;
 		array_index = str_pipe->fd_array_num;
 	}
-	if (node->left->parse_identifier == PARSE_CMD
-		&& node->right->parse_identifier == PARSE_CMD)
+	if ((node->left->parse_identifier == PARSE_CMD
+			&& node->right->parse_identifier == PARSE_CMD)
+		&& node->right->simple_or_pipe != EXIT_PIPE)
 	{
 		pipe(str_pipe->fd_arrays[array_index]);
-        str_pipe->fd_array_num++;
+		str_pipe->fd_array_num++;
 	}
 }
 
-void    close_pipes(t_pipex *str_pipe, t_ast *node)
+void	close_pipes(t_pipex *str_pipe, t_ast *node)
 {
-    unsigned int array_index;
+	unsigned int	array_index;
 
-    array_index = str_pipe->fd_array_num;
-    if (node->left->simple_or_pipe == ENTRY_PIPE || node->right->simple_or_pipe == MIDDLE_PIPE)
-    {
-        close(str_pipe->fd_arrays[array_index - 2][READ]);
-        close(str_pipe->fd_arrays[array_index - 1][WRITE]);
-    }
-    else if (node->right->simple_or_pipe == EXIT_PIPE)
-        close(str_pipe->fd_arrays[array_index - 1][READ]);
+	array_index = str_pipe->fd_array_num;
+	if ((node->left->simple_or_pipe == ENTRY_PIPE
+			&& node->right->simple_or_pipe != EXIT_PIPE)
+		|| node->right->simple_or_pipe == MIDDLE_PIPE)
+	{
+		close(str_pipe->fd_arrays[array_index - 2][READ]);
+		close(str_pipe->fd_arrays[array_index - 1][WRITE]);
+	}
+	else if (node->right->simple_or_pipe == EXIT_PIPE
+		&& node->left->parse_identifier == PARSE_PIPE)
+		close(str_pipe->fd_arrays[array_index - 1][READ]);
+	else
+	{
+		close(str_pipe->fd_arrays[0][READ]);
+		close(str_pipe->fd_arrays[0][WRITE]);
+	}
 }
